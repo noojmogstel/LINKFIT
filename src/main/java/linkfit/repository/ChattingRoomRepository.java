e61e83f7cde86f691a65520a1539d97c4c5ae252
noojmogstel <goaudgo@naver.com>
2024-10-16 22:59:27 +0900

    feat: add Message and ChattingRoom Entity

---
+++ src/main/java/linkfit/repository/ChattingRoomRepository.java
@@ -0,0 +1,10 @@
+package linkfit.repository;
+
+import linkfit.entity.ChattingRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
+
+}

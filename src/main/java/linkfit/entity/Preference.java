d84e82bf9372eb54bf602b73f51b0fa56623c4c6
noojmogstel <goaudgo@naver.com>
2024-11-11 11:18:36 +0900

    fix: modify Preferences field

--- src/main/java/linkfit/entity/Preference.java
+++ src/main/java/linkfit/entity/Preference.java
@@ -37,7 +37,7 @@ public class Preference {
     @Enumerated(EnumType.STRING)
     private TrainerGender gender;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "`range`")
     private Integer range;
 
     @Column(nullable = false)
